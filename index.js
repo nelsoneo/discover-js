91dd7f37d99c362ff0a04309163e5d72291af705
nelsoneo <nelsoneo@gmail.com>
2022-02-28 23:48:00 -0300

    more excercies

--- index.js
+++ index.js
@@ -12,25 +12,31 @@ F = C * 9/5 + 32
 */
 
 function changeTemperature(grade) {
-  const celcius = !grade.toLowerCase().includes("f");
-  const fahrenheit = !grade.toLowerCase().includes("c");
-  const valor = grade.slice(0, grade.length - 1);
+  const celciusExist = grade.toLowerCase().includes("c");
+  const fahrenheitExist = grade.toLowerCase().includes("f");
 
-  if (celcius && fahrenheit) {
-    throw new Error("the letter information not match with celcius or fahrenheit");
-  } else {
-      if (celcius){
-          
-          return 'valor em celcius é : ' + valor * 9/5 + 32;
-      }
-      return 'valor em fahrenheit é : ' + (valor - 32) + 5/9;
+  if (!celciusExist && !fahrenheitExist) {
+    throw new Error(
+      "the letter information not match with celcius or fahrenheit"
+    );
   }
+
+  let valor = grade.match(/\d+/g);
+  let formula = fahrenheit => (fahrenheit - 32) * 5/9;
+  let degreeLetter = "F";
+
+  if (celciusExist) {
+    valor = grade.match(/\d+/g);
+    formula = celcius => celcius * 9/5 + 32;
+    degreeLetter = "C";
+  }
+
+  return formula(valor)+ " " + degreeLetter;
 }
 
 try {
-    changeTemperature("300a")
+  console.log(changeTemperature("10C"));
+  console.log(changeTemperature("50F"));
 } catch (error) {
-    console.log(error.message)
+  console.log(error.message);
 }
-
-
